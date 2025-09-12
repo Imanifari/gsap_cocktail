74e7bc730e21e737a864c797a4a4dc5dd5d39945
Imanifari <imaniahweb@gmail.com>
2025-09-11 13:23:01 -1100

    added all resouces needed for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
-  return <div>welcome</div>;
+  return <Navbar />;
 };
 
 export default App;
